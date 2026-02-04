e931749455babb083118ccda5c1fc6123d0c435d
itsblocky12 <itsblocky12@gmail.com>
2026-02-04 14:53:18 +0100

    Update FarLandsPlugin.java

--- src/main/java/me/yourname/farlands/FarLandsPlugin.java
+++ src/main/java/me/yourname/farlands/FarLandsPlugin.java
@@ -1,1 +1,12 @@
+package me.yourname.farlands;
 
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FarLandsPlugin extends JavaPlugin {
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new FarLandsChunkGenerator();
+    }
+}
